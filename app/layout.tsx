844061a8eb902cbf5abc6c6f7a51166cd0b73996
maria.yun <maria.yun@ostrovok.ru>
2026-01-22 00:11:44 +0500

    v1

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-mashka kakashka
+mashka kakashka v1
       </body>
     </html>
   );
